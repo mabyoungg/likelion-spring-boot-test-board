d33a3fdab713b40493145a7fefcafda4786ae80c
mabyoungg <mabyoungg@gmail.com>
2023-12-04 23:10:53 +0900

    feat: add HomeController

---
+++ src/main/java/com/example/likelionspringbootboard/domain/home/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.likelionspringbootboard.domain.home;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/sbb")
+    @ResponseBody
+    public String index() {
+        return "hi";
+    }
+}
