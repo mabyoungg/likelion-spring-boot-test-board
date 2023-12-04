9b81177653330fb54adf5bc42c04aef6af84fdfc
mabyoungg <mabyoungg@gmail.com>
2023-12-04 22:32:35 +0900

    init: initial project setup

---
+++ src/main/java/com/example/likelionspringbootboard/LikelionSpringBootBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.likelionspringbootboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikelionSpringBootBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LikelionSpringBootBoardApplication.class, args);
+    }
+
+}
