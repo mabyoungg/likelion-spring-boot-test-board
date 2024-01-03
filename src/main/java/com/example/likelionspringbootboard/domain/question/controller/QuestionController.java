0c0a829b6e03e9e5799dfd026c25c179916ee7fa
mabyoungg <mabyoungg@gmail.com>
2024-01-03 16:33:10 +0900

    feat: question detail 추가

--- src/main/java/com/example/likelionspringbootboard/domain/question/controller/QuestionController.java
+++ src/main/java/com/example/likelionspringbootboard/domain/question/controller/QuestionController.java
@@ -7,19 +7,29 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
+@RequestMapping("/question")
 @Controller
 @RequiredArgsConstructor
 public class QuestionController {
     private final QuestionService questionService;
 
-    @GetMapping("/question/list")
-    public String getList(Model model){
+    @GetMapping("/list")
+    public String list(Model model){
         List<Question> questionList = this.questionService.getList();
         model.addAttribute("questionList", questionList);
 
         return "question_list";
+    }
+
+    @GetMapping("/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id) {
+        Question question = questionService.getQuestion(id);
+        model.addAttribute("question",question);
+        return "question_detail";
     }
 }
--- src/main/java/com/example/likelionspringbootboard/domain/question/service/QuestionService.java
+++ src/main/java/com/example/likelionspringbootboard/domain/question/service/QuestionService.java
@@ -1,11 +1,13 @@
 package com.example.likelionspringbootboard.domain.question.service;
 
+import com.example.likelionspringbootboard.domain.exception.DataNotFoundException;
 import com.example.likelionspringbootboard.domain.question.entity.Question;
 import com.example.likelionspringbootboard.domain.question.repository.QuestionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,5 +16,14 @@ public class QuestionService {
 
     public List<Question> getList() {
         return this.questionRepository.findAll();
+    }
+
+    public Question getQuestion(Integer id) {
+        Optional<Question> question = this.questionRepository.findById(id);
+        if(question.isPresent()) {
+            return question.get();
+        } else {
+            throw new DataNotFoundException("question not found");
+        }
     }
 }
