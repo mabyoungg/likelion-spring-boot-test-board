5bfb4b44d1af3768ec386c52eacb2c16500ff912
mabyoungg <mabyoungg@gmail.com>
2024-01-04 11:17:15 +0900

    test: test data 생성

--- src/test/java/com/example/likelionspringbootboard/LikelionSpringBootBoardApplicationTests.java
+++ src/test/java/com/example/likelionspringbootboard/LikelionSpringBootBoardApplicationTests.java
@@ -1,13 +1,23 @@
 package com.example.likelionspringbootboard;
 
+import com.example.likelionspringbootboard.domain.question.service.QuestionService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class LikelionSpringBootBoardApplicationTests {
 
+    @Autowired
+    private QuestionService questionService;
+
     @Test
-    void contextLoads() {
+    void testJpa() {
+        for (int i = 1; i <= 300; i++) {
+            String subject = String.format("테스트 데이터입니다:[%03d]", i);
+            String content = String.format("내용:[%03d]", i);
+            this.questionService.create(subject, content);
+        }
     }
 
 }
