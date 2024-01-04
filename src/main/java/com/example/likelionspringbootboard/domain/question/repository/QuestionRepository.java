17ea826fc5b6e0331abf467d8eb81a2cbf195362
mabyoungg <mabyoungg@gmail.com>
2024-01-04 11:32:07 +0900

    feat: paging 추가

--- src/main/java/com/example/likelionspringbootboard/domain/question/repository/QuestionRepository.java
+++ src/main/java/com/example/likelionspringbootboard/domain/question/repository/QuestionRepository.java
@@ -1,7 +1,10 @@
 package com.example.likelionspringbootboard.domain.question.repository;
 
 import com.example.likelionspringbootboard.domain.question.entity.Question;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
+    Page<Question> findAll(Pageable pageable);
 }
--- src/main/java/com/example/likelionspringbootboard/domain/question/service/QuestionService.java
+++ src/main/java/com/example/likelionspringbootboard/domain/question/service/QuestionService.java
@@ -4,6 +4,9 @@ import com.example.likelionspringbootboard.domain.exception.DataNotFoundExceptio
 import com.example.likelionspringbootboard.domain.question.entity.Question;
 import com.example.likelionspringbootboard.domain.question.repository.QuestionRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -15,8 +18,9 @@ import java.util.Optional;
 public class QuestionService {
     private final QuestionRepository questionRepository;
 
-    public List<Question> getList() {
-        return this.questionRepository.findAll();
+    public Page<Question> getList(int page) {
+        Pageable pageable = PageRequest.of(page, 10);
+        return this.questionRepository.findAll(pageable);
     }
 
     public Question getQuestion(Integer id) {
